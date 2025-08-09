764f15b5a1bc0e01b8663ce11ab7e1cfcbea10db
Cheloniid <61048208+Cheloniid@users.noreply.github.com>
2025-08-10 00:02:01 +0200

    Initial commit

---
+++ src/main/java/com/michal/github_api_task/GithubApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.michal.github_api_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApiTaskApplication.class, args);
+	}
+
+}
