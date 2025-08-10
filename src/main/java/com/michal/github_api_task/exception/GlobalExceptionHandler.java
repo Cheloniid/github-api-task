50556a6b5172210a75eec17643f860b8ff69b7c1
Cheloniid <61048208+Cheloniid@users.noreply.github.com>
2025-08-10 21:57:57 +0200

    change 404 json field order

--- src/main/java/com/michal/github_api_task/exception/GlobalExceptionHandler.java
+++ src/main/java/com/michal/github_api_task/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -14,7 +15,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserNotFound(UserNotFoundException exception) {
 
-        Map<String, String> response = new HashMap<>();
+        Map<String, String> response = new LinkedHashMap<>();
         response.put("status", "404");
         response.put("message", exception.getMessage());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
